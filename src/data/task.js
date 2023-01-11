f1d980b04b09af11a9a1c9020d09a54fa7278f6b
Cristoph23 <59851038+Cristoph23@users.noreply.github.com>
2023-01-11 16:04:39 -0600

    first commit

---
+++ src/data/task.js
@@ -0,0 +1,20 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'Mi primer Tarea',
+        description: 'mi primer tareas'
+    },
+
+    {
+        id: 1,
+        title: 'Mi segunda Tarea',
+        description: 'mi segunda tareas'
+    },
+
+    {
+        id: 2,
+        title: 'Mi tercer Tarea',
+        description: 'mi tercer tareas'
+    }
+]
+
